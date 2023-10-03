b72acf11efcdcadfe51d64b22cda7079941950b9
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-10-03 00:54:46 -0400

    add orchid collection ,js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import myImg from "../../images/my-image.png";
+import myImg from "../images/my-image.png";
 
 function About() {
   const scrollToBottom = () => {
---
+++ src/components/OrchidCollection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function OrchidCollection({ orchids, setIsEditing, isEditing }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="orchid-container">
+      <h2>My Orchid Collection</h2>
+      <ul className="orchid-list">
+        {orchids.map((orchid) => (
+          <li key={orchid.id}>
+            <Link
+              to={`/orchidcollection/${orchid.id}`}
+              onClick={() => {
+                setIsEditing(false);
+                navigate(`/orchidcollection/${orchid.id}`);
+              }}
+            >
+              <div className="orchid-icon">
+                <img src={orchid.image} alt={orchid.name} />
+              </div>
+            </Link>
+            <h5>{orchid.name}</h5>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default OrchidCollection;
