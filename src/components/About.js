2b1ffa2368f690873e5d2bd2f7a225a069f3a491
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-10-03 00:27:27 -0400

    add About.js

---
+++ src/components/About.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import myImg from "../images/my-image.png";
+function About() {
+  const scrollToBottom = () => {
+    const bottomElement = document.getElementById("bottom");
+    if (bottomElement) {
+      bottomElement.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+  };
+
+  return (
+    <div className="about">
+      <div className="orchid-about">
+        <div className="about-orchid-text">
+          <h2>About the World of Orchids</h2>
+          <p>
+            Orchids are a diverse and fascinating group of flowering plants
+            known for their stunning and unique blooms. With over 25,000
+            species, orchids can be found in nearly every corner of the world,
+            from lush rainforests to arid deserts.
+          </p>
+          <p>
+            As a plant biologist with a Ph.D. in plant biology, I've dedicated
+            my life to understanding the intricate beauty and ecological
+            significance of orchids. Through this project, I aim to share my
+            passion for these remarkable plants with the world.
+          </p>
+        </div>
+        <div className="about-orchid-image">
+          <img
+            src="https://your-orchid-image-url.com"
+            alt="orchid"
+          />
+        </div>
+      </div>
+      <div className="me-about">
+        <div className="about-me-image">
+          <img src={myImg} alt="me" />
+        </div>
+        <div className="about-me-text">
+          <h2>About Me</h2>
+          <p>
+            I'm an avid orchid enthusiast with a deep love for botanical
+            exploration and cultivation. Through this platform, I invite you to
+            explore the enchanting world of orchids with me.
+          </p>
+          <p>Discover unique orchid varieties and share your own orchid stories!</p>
+        </div>
+      </div>
+      <div className="about-links">
+        <Link to="/about/contactme" onClick={scrollToBottom}>
+          Get in Touch
+        </Link>
+        <Link to="/about/faqs" onClick={scrollToBottom}>
+          FAQs
+        </Link>
+      </div>
+      <Outlet />
+      <div id="bottom" style={{ paddingBottom: "30vh" }} />
+    </div>
+  );
+}
+
+export default About;
