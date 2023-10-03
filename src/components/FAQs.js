e1d35c7ddf8e412d9547092b283e35b7ae64519e
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-10-03 12:27:26 -0400

    add my FAQ

---
+++ src/components/FAQs.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function FAQs() {
+  return (
+    <div className="FAQ">
+      <h2>Frequently Asked Questions</h2>
+      <div className="wrapper">
+        <h3>What is your app all about?</h3>
+        <p>I'm passionate about Orchids, and I created this platform to share my love for these beautiful plants!</p>
+      </div>
+      <div className="wrapper">
+        <h3>Do you own all these orchid plants?</h3>
+        <p>While I don't own all of them, I do have my own collection of orchids that I cherish.</p>
+      </div>
+      <div className="wrapper">
+        <h3>Can I suggest a new orchid to add to your collection?</h3>
+        <p>Absolutely! Feel free to click on "Add an Orchid" above and send me your suggestion!</p>
+      </div>
+      <div className="wrapper">
+        <h3>How can I get in touch with you?</h3>
+        <p>You can reach out to me by clicking the "Contact Me" link above!</p>
+      </div>
+    </div>
+  );
+}
+
+export default FAQs;
